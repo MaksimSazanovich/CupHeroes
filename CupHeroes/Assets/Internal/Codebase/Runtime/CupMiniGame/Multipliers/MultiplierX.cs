a38ac076b612234ee95dab45915cbb6af5c44fc5
MaksimSazanovich <sazanovichmaksim@gmail.com>
2024-06-16 21:57:18 +0300

    Add X multiplaires

---
+++ CupHeroes/Assets/Internal/Codebase/Runtime/CupMiniGame/Multipliers/MultiplierX.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Internal.Codebase.Runtime.CupMiniGame.Multipliers
+{
+    [DisallowMultipleComponent]
+    public sealed class MultiplierX : MonoBehaviour
+    {
+        [SerializeField] private int value;
+    }
+}
